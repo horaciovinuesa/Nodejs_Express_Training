7917b77edecf42ccf416bd6f93b05a0b74f0d828
horaciovinuesa <horaciovinuesa@gmail.com>
2023-01-17 09:24:55 -0300

    Starting with routing

---
+++ Express/app.js
@@ -0,0 +1,33 @@
+    const http = require ('http');
+    const courses = require('./courses');
+
+const server = http.createServer((req, res) =>{
+    const {method} = req;
+
+    switch(method){
+        case 'GET':
+            return handleGET(req, res);
+        
+        
+        default:
+            console.log(`The requested method could not be handled by server: ${method}`);
+    }
+});
+
+function handleGET(req, res){
+    let path = req.url;
+
+    if(path === '/'){
+        res.statusCode = 200;
+        res.end('Welcome to my first server and API created with NodeJS');
+    }else if(path === '/courses'){
+        res.statusCode = 200;
+        res.end(JSON.stringify(courses.infoCourses))
+    }
+}
+
+const PORT = 3000;
+
+server.listen(() => {
+    console.log(`Server is listening at port ${PORT}...`);
+});
---
+++ Express/courses.js
@@ -0,0 +1,43 @@
+let infoCourses = {
+    'programming': [
+        {
+            id: 1,
+            title: 'Learn Python',
+            language: 'python',
+            views: 12000,
+            level: basic
+        },
+        {
+            id: 2,
+            title: 'Python lvl2',
+            language: 'python',
+            views: 4500,
+            level: intermediate
+        },
+        {
+            id: 3,
+            title: 'JS lvl2',
+            language: 'javascript',
+            views: 9875,
+            level: advanced
+        }
+    ],
+    'math': [
+        {
+            id: 1,
+            title: 'Learn Math',
+            language: 'school',
+            views: 116548,
+            level: basic
+        },
+        {
+            id: 2,
+            title: 'Math lvl2',
+            language: 'school',
+            views: 9875,
+            level: intermediate
+        }
+    ]
+}
+
+module.export.infoCourses = infoCourses;
